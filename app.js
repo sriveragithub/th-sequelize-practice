72eb31f9c210e227b3c6716c9edcbd98b906e476
Skyler Rivera <skylerrivera17@yahoo.com>
2021-08-19 16:06:33 -0400

    creating base sequelize info for simple movies table in app.js

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
+// Why: creating sequelize instance to create a movies database
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'movies.db'
+  dialect: "sqlite",
+  storage: "movies.db",
 });
 
+// Why: creating a new table in sequelize and setting it's columns
+class Movie extends Sequelize.Model {}
+Movie.init(
+  {
+    title: Sequelize.STRING,
+  },
+  { sequelize }
+);
+
 // async IIFE
+// Why: populate the new table with seed info so that we can test
 (async () => {
+  await sequelize.sync({ force: true }); // tells the database to drop the table before creating new table IF NOT EXISTS
+
   try {
-    await sequelize.authenticate()
-    console.log('Connection to database successful!');
+    const movieInstances = await Promise.all([
+      Movie.create({
+        title: 'Toy Story'
+      }),
+      Movie.create({
+        title: 'The Incredibles'
+      }),
+    ])
+    const moviesJSON = movieInstances.map(movie => movie.toJSON())
+    console.log(moviesJSON);
   } catch (err) {
-    console.error('Error connecting to the database: ', err)
+    console.error("Error connecting to the database: ", err);
   }
 })();
